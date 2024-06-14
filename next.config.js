a2d48fbffbf13c063497fc980708ae91e06420a8
Dibya1258 <d.p7787877172@gmail.com>
2024-06-14 16:28:52 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-      // Uncomment and configure domains if you need to handle external images
-      // domains: ['example.com'],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
